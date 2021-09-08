e35ef7e1d343636d00921d0e82425b4c9978fb7b
Rosa Kang <therosessom@gmail.com>
2021-09-08 11:51:40 -0700

    update search function

--- archive-campus.php
+++ archive-campus.php
@@ -6,17 +6,19 @@ pageBanner(array(
 ))
 ?>
 
-<div class="container container--narrow page-section">
+        <div class="container container--narrow page-section">
+                <ul class="link-list min-list">
+                        <?php 
+                        while(have_posts()) {
+                                the_post(); ?> 
+                                <li><a href="<? the_permalink(); ?>"><?php the_title(); ?></a></li>
+                                <?php }
+                        echo paginate_links();
+                        ?>
+                </ul>
+        </div>
+    
+<?php
+  get_footer();
 
-<ul class="link-list min-list">
-        <?php 
-            while(have_posts()) {
-                the_post(); ?> 
-                <li><a href="<? the_permalink(); ?>"><?php the_title(); ?></a></li>
-                <?php }
-        echo paginate_links();
-        ?>
-</ul>
-</div>
-<?php get_footer();
 ?>
